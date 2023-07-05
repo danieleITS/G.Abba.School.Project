a34c1e133fa1aa8f9ab68d31894ef730959db1a3
Piastrelle <fabriziolisci34@gmail.com>
2023-07-05 03:30:36 +0200

    quasi finito (;´ρ`)

--- Form Application/ScuoleGestione/ScuoleGestione/Form1.Designer.cs
+++ Form Application/ScuoleGestione/ScuoleGestione/Form1.Designer.cs
@@ -36,7 +36,7 @@
             this.btnElimina = new System.Windows.Forms.Button();
             this.btnRicerca = new System.Windows.Forms.Button();
             this.btnModifica = new System.Windows.Forms.Button();
-            this.btnCercaCercaStudente = new System.Windows.Forms.Button();
+            this.btn1 = new System.Windows.Forms.Button();
             this.lbl4 = new System.Windows.Forms.Label();
             this.lbl5 = new System.Windows.Forms.Label();
             this.txt2 = new System.Windows.Forms.TextBox();
@@ -45,12 +45,12 @@
             this.lbl2 = new System.Windows.Forms.Label();
             this.btnVisualizzaClassi = new System.Windows.Forms.Button();
             this.btnVisualizzaMaterie = new System.Windows.Forms.Button();
-            this.lblTitolo = new System.Windows.Forms.Label();
+            this.lbl0 = new System.Windows.Forms.Label();
             this.lbl1 = new System.Windows.Forms.Label();
             this.txt1 = new System.Windows.Forms.TextBox();
+            this.btnVisualizzaStudenti = new System.Windows.Forms.Button();
             this.cmb2 = new System.Windows.Forms.ComboBox();
             this.lbl3 = new System.Windows.Forms.Label();
-            this.btnVisualizzaStudenti = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dtgrdvw)).BeginInit();
             this.SuspendLayout();
             // 
@@ -69,16 +69,17 @@
             // lblTestConnessioneDB
             // 
             this.lblTestConnessioneDB.AutoSize = true;
-            this.lblTestConnessioneDB.Location = new System.Drawing.Point(0, 7);
+            this.lblTestConnessioneDB.Location = new System.Drawing.Point(855, 4);
             this.lblTestConnessioneDB.Name = "lblTestConnessioneDB";
             this.lblTestConnessioneDB.Size = new System.Drawing.Size(141, 13);
             this.lblTestConnessioneDB.TabIndex = 2;
             this.lblTestConnessioneDB.Text = "Test Connessione Database";
+            this.lblTestConnessioneDB.Visible = false;
             this.lblTestConnessioneDB.Click += new System.EventHandler(this.lblTestConnessioneDB_Click);
             // 
             // btnVisualizzaRegistro
             // 
-            this.btnVisualizzaRegistro.Location = new System.Drawing.Point(189, 69);
+            this.btnVisualizzaRegistro.Location = new System.Drawing.Point(191, 70);
             this.btnVisualizzaRegistro.Name = "btnVisualizzaRegistro";
             this.btnVisualizzaRegistro.Size = new System.Drawing.Size(150, 30);
             this.btnVisualizzaRegistro.TabIndex = 3;
@@ -94,6 +95,7 @@
             this.dtgrdvw.RowHeadersWidth = 50;
             this.dtgrdvw.Size = new System.Drawing.Size(600, 480);
             this.dtgrdvw.TabIndex = 4;
+            this.dtgrdvw.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dtgrdvw_CellContentClick);
             // 
             // btnInserisci
             // 
@@ -139,75 +141,82 @@
             this.btnModifica.UseVisualStyleBackColor = true;
             this.btnModifica.Click += new System.EventHandler(this.btnModifica_Click);
             // 
-            // btnCercaCercaStudente
+            // btn1
             // 
-            this.btnCercaCercaStudente.Location = new System.Drawing.Point(127, 398);
-            this.btnCercaCercaStudente.Margin = new System.Windows.Forms.Padding(2);
-            this.btnCercaCercaStudente.Name = "btnCercaCercaStudente";
-            this.btnCercaCercaStudente.Size = new System.Drawing.Size(56, 20);
-            this.btnCercaCercaStudente.TabIndex = 7;
-            this.btnCercaCercaStudente.Text = "Cerca";
-            this.btnCercaCercaStudente.UseVisualStyleBackColor = true;
-            this.btnCercaCercaStudente.Click += new System.EventHandler(this.btnCerca_Click);
+            this.btn1.Location = new System.Drawing.Point(138, 388);
+            this.btn1.Margin = new System.Windows.Forms.Padding(2);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(100, 25);
+            this.btn1.TabIndex = 7;
+            this.btn1.Text = "Cerca";
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Visible = false;
+            this.btn1.Click += new System.EventHandler(this.btnCerca_Click);
             // 
             // lbl4
             // 
             this.lbl4.AutoSize = true;
-            this.lbl4.Location = new System.Drawing.Point(82, 341);
+            this.lbl4.Location = new System.Drawing.Point(30, 319);
             this.lbl4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lbl4.Name = "lbl4";
             this.lbl4.Size = new System.Drawing.Size(35, 13);
             this.lbl4.TabIndex = 8;
             this.lbl4.Text = "Nome";
+            this.lbl4.Visible = false;
             // 
             // lbl5
             // 
             this.lbl5.AutoSize = true;
-            this.lbl5.Location = new System.Drawing.Point(84, 369);
+            this.lbl5.Location = new System.Drawing.Point(30, 349);
             this.lbl5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lbl5.Name = "lbl5";
             this.lbl5.Size = new System.Drawing.Size(52, 13);
             this.lbl5.TabIndex = 9;
             this.lbl5.Text = "Cognome";
+            this.lbl5.Visible = false;
             // 
             // txt2
             // 
-            this.txt2.Location = new System.Drawing.Point(142, 341);
+            this.txt2.Location = new System.Drawing.Point(117, 316);
             this.txt2.Margin = new System.Windows.Forms.Padding(2);
             this.txt2.Name = "txt2";
-            this.txt2.Size = new System.Drawing.Size(76, 20);
+            this.txt2.Size = new System.Drawing.Size(222, 20);
             this.txt2.TabIndex = 11;
+            this.txt2.Visible = false;
             // 
             // txt3
             // 
-            this.txt3.Location = new System.Drawing.Point(144, 369);
+            this.txt3.Location = new System.Drawing.Point(117, 346);
             this.txt3.Margin = new System.Windows.Forms.Padding(2);
             this.txt3.Name = "txt3";
-            this.txt3.Size = new System.Drawing.Size(76, 20);
+            this.txt3.Size = new System.Drawing.Size(222, 20);
             this.txt3.TabIndex = 12;
+            this.txt3.Visible = false;
             // 
             // cmb1
             // 
             this.cmb1.FormattingEnabled = true;
-            this.cmb1.Location = new System.Drawing.Point(144, 280);
+            this.cmb1.Location = new System.Drawing.Point(117, 254);
             this.cmb1.Margin = new System.Windows.Forms.Padding(2);
             this.cmb1.Name = "cmb1";
-            this.cmb1.Size = new System.Drawing.Size(92, 21);
+            this.cmb1.Size = new System.Drawing.Size(222, 21);
             this.cmb1.TabIndex = 14;
+            this.cmb1.Visible = false;
             // 
             // lbl2
             // 
             this.lbl2.AutoSize = true;
-            this.lbl2.Location = new System.Drawing.Point(84, 280);
+            this.lbl2.Location = new System.Drawing.Point(30, 257);
             this.lbl2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lbl2.Name = "lbl2";
             this.lbl2.Size = new System.Drawing.Size(42, 13);
             this.lbl2.TabIndex = 15;
             this.lbl2.Text = "Materia";
+            this.lbl2.Visible = false;
             // 
             // btnVisualizzaClassi
             // 
-            this.btnVisualizzaClassi.Location = new System.Drawing.Point(33, 105);
+            this.btnVisualizzaClassi.Location = new System.Drawing.Point(35, 106);
             this.btnVisualizzaClassi.Name = "btnVisualizzaClassi";
             this.btnVisualizzaClassi.Size = new System.Drawing.Size(150, 30);
             this.btnVisualizzaClassi.TabIndex = 16;
@@ -217,7 +226,7 @@
             // 
             // btnVisualizzaMaterie
             // 
-            this.btnVisualizzaMaterie.Location = new System.Drawing.Point(189, 105);
+            this.btnVisualizzaMaterie.Location = new System.Drawing.Point(191, 106);
             this.btnVisualizzaMaterie.Name = "btnVisualizzaMaterie";
             this.btnVisualizzaMaterie.Size = new System.Drawing.Size(150, 30);
             this.btnVisualizzaMaterie.TabIndex = 17;
@@ -225,30 +234,45 @@
             this.btnVisualizzaMaterie.UseVisualStyleBackColor = true;
             this.btnVisualizzaMaterie.Click += new System.EventHandler(this.btnVisualizzaMaterie_Click);
             // 
-            // lblTitolo
+            // lbl0
             // 
-            this.lblTitolo.AutoSize = true;
-            this.lblTitolo.Location = new System.Drawing.Point(127, 239);
-            this.lblTitolo.Name = "lblTitolo";
-            this.lblTitolo.Size = new System.Drawing.Size(79, 13);
-            this.lblTitolo.TabIndex = 18;
-            this.lblTitolo.Text = "Cerca studente";
+            this.lbl0.AutoSize = true;
+            this.lbl0.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F);
+            this.lbl0.Location = new System.Drawing.Point(127, 186);
+            this.lbl0.Name = "lbl0";
+            this.lbl0.Size = new System.Drawing.Size(132, 22);
+            this.lbl0.TabIndex = 18;
+            this.lbl0.Text = "Cerca studente";
+            this.lbl0.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lbl0.Visible = false;
             // 
             // lbl1
             // 
             this.lbl1.AutoSize = true;
-            this.lbl1.Location = new System.Drawing.Point(88, 258);
+            this.lbl1.Location = new System.Drawing.Point(30, 228);
             this.lbl1.Name = "lbl1";
             this.lbl1.Size = new System.Drawing.Size(50, 13);
             this.lbl1.TabIndex = 20;
             this.lbl1.Text = "Matricola";
+            this.lbl1.Visible = false;
             // 
             // txt1
             // 
-            this.txt1.Location = new System.Drawing.Point(144, 255);
+            this.txt1.Location = new System.Drawing.Point(117, 225);
             this.txt1.Name = "txt1";
-            this.txt1.Size = new System.Drawing.Size(100, 20);
+            this.txt1.Size = new System.Drawing.Size(222, 20);
             this.txt1.TabIndex = 21;
+            this.txt1.Visible = false;
+            // 
+            // btnVisualizzaStudenti
+            // 
+            this.btnVisualizzaStudenti.Location = new System.Drawing.Point(35, 70);
+            this.btnVisualizzaStudenti.Name = "btnVisualizzaStudenti";
+            this.btnVisualizzaStudenti.Size = new System.Drawing.Size(150, 30);
+            this.btnVisualizzaStudenti.TabIndex = 38;
+            this.btnVisualizzaStudenti.Text = "Visualizza Studenti";
+            this.btnVisualizzaStudenti.UseVisualStyleBackColor = true;
+            this.btnVisualizzaStudenti.Click += new System.EventHandler(this.btnVisualizzaStudenti_Click);
             // 
             // cmb2
             // 
@@ -291,28 +315,21 @@
             "1½",
             "1+",
             "1"});
-            this.cmb2.Location = new System.Drawing.Point(123, 306);
+            this.cmb2.Location = new System.Drawing.Point(117, 285);
             this.cmb2.Name = "cmb2";
-            this.cmb2.Size = new System.Drawing.Size(121, 21);
+            this.cmb2.Size = new System.Drawing.Size(222, 21);
             this.cmb2.TabIndex = 22;
+            this.cmb2.Visible = false;
             // 
             // lbl3
             // 
             this.lbl3.AutoSize = true;
-            this.lbl3.Location = new System.Drawing.Point(78, 309);
+            this.lbl3.Location = new System.Drawing.Point(30, 288);
             this.lbl3.Name = "lbl3";
             this.lbl3.Size = new System.Drawing.Size(29, 13);
             this.lbl3.TabIndex = 23;
             this.lbl3.Text = "Voto";
-            // 
-            // btnVisualizzaStudenti
-            // 
-            this.btnVisualizzaStudenti.Location = new System.Drawing.Point(33, 69);
-            this.btnVisualizzaStudenti.Name = "btnVisualizzaStudenti";
-            this.btnVisualizzaStudenti.Size = new System.Drawing.Size(150, 30);
-            this.btnVisualizzaStudenti.TabIndex = 38;
-            this.btnVisualizzaStudenti.Text = "Visualizza Studenti";
-            this.btnVisualizzaStudenti.UseVisualStyleBackColor = true;
+            this.lbl3.Visible = false;
             // 
             // Form1
             // 
@@ -324,7 +341,7 @@
             this.Controls.Add(this.cmb2);
             this.Controls.Add(this.txt1);
             this.Controls.Add(this.lbl1);
-            this.Controls.Add(this.lblTitolo);
+            this.Controls.Add(this.lbl0);
             this.Controls.Add(this.btnVisualizzaMaterie);
             this.Controls.Add(this.btnVisualizzaClassi);
             this.Controls.Add(this.lbl2);
@@ -333,7 +350,7 @@
             this.Controls.Add(this.txt2);
             this.Controls.Add(this.lbl5);
             this.Controls.Add(this.lbl4);
-            this.Controls.Add(this.btnCercaCercaStudente);
+            this.Controls.Add(this.btn1);
             this.Controls.Add(this.btnModifica);
             this.Controls.Add(this.btnRicerca);
             this.Controls.Add(this.btnElimina);
@@ -360,7 +377,7 @@
         private System.Windows.Forms.DataGridView dtgrdvw;
         private System.Windows.Forms.Button btnInserisci;
         private System.Windows.Forms.Button btnElimina;
-        private System.Windows.Forms.Button btnCercaCercaStudente;
+        private System.Windows.Forms.Button btn1;
         private System.Windows.Forms.Label lbl4;
         private System.Windows.Forms.Label lbl5;
         private System.Windows.Forms.TextBox txt2;
@@ -371,12 +388,12 @@
         private System.Windows.Forms.Button btnModifica;
         private System.Windows.Forms.Button btnVisualizzaClassi;
         private System.Windows.Forms.Button btnVisualizzaMaterie;
-        private System.Windows.Forms.Label lblTitolo;
+        private System.Windows.Forms.Label lbl0;
         private System.Windows.Forms.Label lbl1;
         private System.Windows.Forms.TextBox txt1;
+        private System.Windows.Forms.Button btnVisualizzaStudenti;
         private System.Windows.Forms.ComboBox cmb2;
         private System.Windows.Forms.Label lbl3;
-        private System.Windows.Forms.Button btnVisualizzaStudenti;
     }
 }
 
--- Form Application/ScuoleGestione/ScuoleGestione/Form1.cs
+++ Form Application/ScuoleGestione/ScuoleGestione/Form1.cs
@@ -14,16 +14,18 @@ namespace ScuoleGestione
 {
     public partial class Form1 : Form
     {
+        private string table = "";
+
         public Form1()
         {
             InitializeComponent();
             dtgrdvw.AllowUserToAddRows = false;
             dtgrdvw.AllowUserToDeleteRows = false;
-            /*
+            
             DataSet ds = new DataSet();
             dtgrdvw.AutoGenerateColumns = true;
             dtgrdvw.DataSource = ds;
-
+            /*
             var cs = "Host=192.168.11.17; Username=postgres; Password=1234abcd; Database=gabba_DB";
             var con = new NpgsqlConnection(cs);
             con.Open();
@@ -52,22 +54,6 @@ namespace ScuoleGestione
                 cmbClasseInserisciStudente.Items.Add(drd["classe"].ToString());
             }
             */
-            HideElement();
-        }
-
-        private void HideElement()
-        {
-            lbl4.Visible = false;
-            lbl5.Visible = false;
-            txt2.Visible = false;
-            txt3.Visible = false;
-            btnCercaCercaStudente.Visible = false;
-            lbl1.Visible = false;
-            lbl3.Visible = false;
-            lbl2.Visible = false;
-            txt1.Visible = false;
-            cmb2.Visible = false;
-            cmb1.Visible = false;
         }
 
         private void lblTestConnessioneDB_Click(object sender, EventArgs e)
@@ -75,33 +61,15 @@ namespace ScuoleGestione
             
         }
 
-        private void btnVisualizzaRegistro_Click(object sender, EventArgs e)
-        {
-            // visualizzazione del registro completo con un click
-        }
+        
 
-        private void btnVisualizzaClassi_Click(object sender, EventArgs e)
-        {
-            // visualizza le classi presenti nella scuola
-        }
+        
 
-        private void btnVisualizzaMaterie_Click(object sender, EventArgs e)
-        {
-            // visualizza le materie presenti nella scuola
-        }
+        
 
-        private void btnInserisci_Click(object sender, EventArgs e)
-        {
-            // inserimento nuova riga nella tabella visualizzata
-            // con dati studente (nome, cognome, matricola), classe (anno, sezione), voto (materia, voto);
-            // trasformare le label con qualcosa di sensato in base a cosa bisogna inserire
-        }
+        
 
-        private void btnRicerca_Click(object sender, EventArgs e)
-        {
-            // ricerca di elementi nella tabella visualizzata
-            // trasformare le label con qualcosa di sensato in base a cosa si può cercare
-        }
+        
 
         private void btnCerca_Click(object sender, EventArgs e)
         {
@@ -182,7 +150,7 @@ namespace ScuoleGestione
 
         private void Form1_Load(object sender, EventArgs e)
         {
-
+            HideForm();
         }
 
         private void btnInserisciStudente_Click(object sender, EventArgs e)
@@ -205,5 +173,289 @@ namespace ScuoleGestione
             }
         }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        private void btnVisualizzaStudenti_Click(object sender, EventArgs e)
+        {
+            HideForm();
+            Enable("Studente");
+
+            /*
+            var con = new NpgsqlConnection("Host=192.168.11.17; Username=postgres; Password=1234abcd; Database=gabba_DB");
+            con.Open();
+
+            var sql = "SELECT * FROM studenti";
+
+            NpgsqlCommand cmd = new NpgsqlCommand(sql, con);
+            NpgsqlDataReader reader = cmd.ExecuteReader();
+            DataTable table = new DataTable();
+            table.Load(reader);
+            dtgrdvw.DataSource = table;
+
+            con.Close();
+            */
+        }
+
+        private void btnVisualizzaClassi_Click(object sender, EventArgs e)
+        {
+            HideForm();
+            Enable("Classe");
+
+            /*
+            var con = new NpgsqlConnection("Host=192.168.11.17; Username=postgres; Password=1234abcd; Database=gabba_DB");
+            con.Open();
+
+            var sql = "SELECT * FROM classi";
+
+            NpgsqlCommand cmd = new NpgsqlCommand(sql, con);
+            NpgsqlDataReader reader = cmd.ExecuteReader();
+            DataTable table = new DataTable();
+            table.Load(reader);
+            dtgrdvw.DataSource = table;
+
+            con.Close();
+            */
+        }
+
+        private void btnVisualizzaRegistro_Click(object sender, EventArgs e)
+        {
+            HideForm();
+            Enable("Voto");
+
+            /*
+            var con = new NpgsqlConnection("Host=192.168.11.17; Username=postgres; Password=1234abcd; Database=gabba_DB");
+            con.Open();
+
+            var sql = "SELECT * FROM voti";
+
+            NpgsqlCommand cmd = new NpgsqlCommand(sql, con);
+            NpgsqlDataReader reader = cmd.ExecuteReader();
+            DataTable table = new DataTable();
+            table.Load(reader);
+            dtgrdvw.DataSource = table;
+
+            con.Close();
+            */
+        }
+
+        private void btnVisualizzaMaterie_Click(object sender, EventArgs e)
+        {
+            HideForm();
+            Enable("Materia");
+
+            /*
+            var con = new NpgsqlConnection("Host=192.168.11.17; Username=postgres; Password=1234abcd; Database=gabba_DB");
+            con.Open();
+
+            var sql = "SELECT * FROM materie";
+
+            NpgsqlCommand cmd = new NpgsqlCommand(sql, con);
+            NpgsqlDataReader reader = cmd.ExecuteReader();
+            DataTable table = new DataTable();
+            table.Load(reader);
+            dtgrdvw.DataSource = table;
+
+            con.Close();
+            */
+        }
+
+        private void btnInserisci_Click(object sender, EventArgs e)
+        {
+            HideForm();
+            ShowForm("Inserisci-" + table);
+        }
+
+        private void btnRicerca_Click(object sender, EventArgs e)
+        {
+            HideForm();
+            ShowForm("Ricerca-" + table);
+        }
+
+        private void dtgrdvw_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            
+        }
+
+        private void HideForm()
+        {
+            btnInserisci.Enabled = false;
+            btnRicerca.Enabled = false;
+            btnModifica.Enabled = false;
+            btnElimina.Enabled = false;
+            lbl0.Visible = false;
+            lbl1.Visible = false;
+            lbl2.Visible = false;
+            lbl3.Visible = false;
+            lbl4.Visible = false;
+            lbl5.Visible = false;
+            txt1.Visible = false;
+            txt2.Visible = false;
+            txt3.Visible = false;
+            cmb1.Visible = false;
+            cmb2.Visible = false;
+            btn1.Visible = false;
+        }
+
+        private void ShowForm(string usage)
+        {
+            switch (usage)
+            {
+                case "Inserisci-Studente":
+                    lbl0.Visible = true;
+                    lbl0.Text = "Inserisci Studente";
+                    lbl1.Visible = true;
+                    lbl1.Text = "Matricola";
+                    lbl2.Visible = true;
+                    lbl2.Text = "Classe";
+                    lbl4.Visible = true;
+                    lbl4.Text = "Nome";
+                    lbl5.Visible = true;
+                    lbl5.Text = "Cognome";
+                    txt1.Visible = true;
+                    txt2.Visible = true;
+                    txt3.Visible = true;
+                    cmb1.Visible = true;
+                    btn1.Visible = true;
+                    btn1.Text = "Inserisci";
+                    break;
+
+                case "Inserisci-Voto":
+                    lbl0.Visible = true;
+                    lbl0.Text = "Inserisci Voto";
+                    lbl1.Visible = true;
+                    lbl1.Text = "Matricola";
+                    lbl2.Visible = true;
+                    lbl2.Text = "Voto";
+                    lbl3.Visible = true;
+                    lbl3.Text = "Materia";
+                    txt1.Visible = true;
+                    cmb1.Visible = true;
+                    cmb2.Visible = true;
+                    btn1.Visible = true;
+                    btn1.Text = "Inserisci";
+                    break;
+
+                case "Inserisci-Classe":
+                    lbl0.Visible = true;
+                    lbl0.Text = "Inserisci Classe";
+                    lbl2.Visible = true;
+                    lbl2.Text = "Anno";
+                    lbl3.Visible = true;
+                    lbl3.Text = "Sezione";
+                    cmb1.Visible = true;
+                    cmb2.Visible = true;
+                    btn1.Visible = true;
+                    btn1.Text = "Inserisci";
+                    break;
+
+                case "Inserisci-Materia":
+                    lbl0.Visible = true;
+                    lbl0.Text = "Inserisci Materia";
+                    lbl1.Visible = true;
+                    lbl1.Text = "Materia";
+                    txt1.Visible = true;
+                    btn1.Visible = true;
+                    btn1.Text = "Inserisci";
+                    break;
+
+                case "Ricerca-Studente":
+                    lbl0.Visible = true;
+                    lbl0.Text = "Ricerca Studente";
+                    lbl1.Visible = true;
+                    lbl1.Text = "Matricola";
+                    lbl2.Visible = true;
+                    lbl2.Text = "Classe";
+                    lbl4.Visible = true;
+                    lbl4.Text = "Nome";
+                    lbl5.Visible = true;
+                    lbl5.Text = "Cognome";
+                    txt1.Visible = true;
+                    txt2.Visible = true;
+                    txt3.Visible = true;
+                    cmb1.Visible = true;
+                    btn1.Visible = true;
+                    btn1.Text = "Ricerca";
+                    break;
+
+                case "Ricerca-Voto":
+                    lbl0.Visible = true;
+                    lbl0.Text = "Ricerca Voto";
+                    lbl1.Visible = true;
+                    lbl1.Text = "Matricola";
+                    txt1.Visible = true;
+                    btn1.Visible = true;
+                    break;
+
+                case "Modifica-Studente":
+                    break;
+
+                case "Modifica-Voto":
+                    break;
+
+                case "Modifica-Materia":
+                    break;
+
+                case "Elimina-Studente":
+                    break;
+
+                case "Elimina-Voto":
+                    break;
+
+                case "Elimina-Classe":
+                    break;
+
+                case "Elimina-Materia":
+                    break;
+            }
+        }
+
+        private void Enable(string button)
+        {
+            switch (button)
+            {
+                case "Studente":
+                    btnInserisci.Enabled = true;
+                    btnRicerca.Enabled = true;
+                    table = "Studente";
+                    break;
+                case "Classe":
+                    btnInserisci.Enabled = true;
+                    table = "Classe";
+                    break;
+                case "Voto":
+                    btnInserisci.Enabled = true;
+                    btnRicerca.Enabled = true;
+                    table = "Voto";
+                    break;
+                case "Materia":
+                    btnInserisci.Enabled = true;
+                    table = "Materia";
+                    break;
+            }
+        }
     }
 }
